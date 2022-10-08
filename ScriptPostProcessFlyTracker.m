fcb01c5688128a77b9721c99006f4a8af4d1f94f
Boaz Mohar <boazmohar@gmail.com>
2022-10-08 16:12:07 -0400

    Added more variables and comments

--- ScriptPostProcessFlyTracker.m
+++ ScriptPostProcessFlyTracker.m
@@ -2,11 +2,11 @@
 
 %% set up paths for code
 
-% add JAABA to the path (.. is the parent directory to the current one)
+% add JAABA to the path (assume it is in the parent directory to the current one)
 current_directory = pwd;
-parent_directory = fileparts(currentdir);
-addpath(fullfile(parentDirectory, 'JAABA', 'misc'));
-addpath(fullfile(parentDirectory, 'JAABA', 'filehandling'));
+parent_directory = fileparts(current_directory);
+addpath(fullfile(parent_directory, 'JAABA', 'misc'));
+addpath(fullfile(parent_directory, 'JAABA', 'filehandling'));
 
 % add path to BIOL 341 code if not in this directory already
 % uncomment this!
@@ -27,16 +27,18 @@ expdirs = cell(size(expnames));
 for i = 1:numel(expnames)
   expdirs{i} = fullfile(rootdatadir,expnames{i});
 end
-disp(expdirs)
+fprintf('Here is the first full exp dir:\n%s\n', expdirs{i})
 %% look at a video
 
 % pick a movie (1 - number of expnames)
 expi = 1;
 expdir = expdirs{expi};
-disp(sprintf('working on exp %s', expdir))
+fprintf('working on exp %s\n', expdir)
 
-% open playfmf with a specific video
-playfmf('moviefile',fullfile(expdir,'movie.ufmf'));
+% open playfmf with a specific video (assume it is called movie.ufmf)
+movie_name = fullfile(expdir,'movie.ufmf');
+fprintf('trying to open movie:\n %s\n', movie_name)
+playfmf('moviefile',movie_name);
 
 % if you call just playfmf it will prompt you for the video
 %playfmf;
@@ -45,46 +47,79 @@ playfmf('moviefile',fullfile(expdir,'movie.ufmf'));
 
 for expi = 1:numel(expdirs)
   expdir = expdirs{expi};
-  fprintf('%d: %s\n',expi,expdir);
+  fprintf('Working on #%d: %s\n',expi,expdir);
   res = PostProcessFlyTracker4JAABA(expdir);
 end
 
 %% plot an example frame with tracking
 
+% set up name and frame to look at
 expi = 1;
-fr = 100;
+fr = 107;
 expdir = expdirs{expi};
-data = LoadTracking(expdir);
+fprintf('working on exp %s\n', expdir)
+movie_name = fullfile(expdir,'movie.ufmf');
+fprintf('trying to open frame #%d from file movie:\n%s\n', fr, movie_name)
 
-[readframe,nframes,fid,headerinfo] = get_readframe_fcn(fullfile(expdir,'movie.ufmf'));
+% get a function (readframe) according to the movie you want to read
+[readframe,nframes,fid,headerinfo] = get_readframe_fcn(movie_name);
+% read the frame
 im = readframe(fr);
+sz = size(im);
+fprintf('Read 1 frame size is %d x %d pixels \n', sz(1), sz(2))
+% close the file (no more reading)
 fclose(fid);
 
-hfig = figure(1);
+% plot the frame in grayscale
+figure(1);
 clf;
-hax = gca;
 imagesc(im,[0,255]);
 colormap gray;
+
+% add tracking information, what is LoadTracking? mark the function and
+% press F1 to get help. Also look at the result 'data' in the Variable
+% explorer
+
+data = LoadTracking(expdir); % explor this struct in the workspace!
 hold on;
 ntrajs = size(data.summary.flies,1);
-colors = jet(ntrajs);
-hbody = gobjects(ntrajs,1);
+fprintf('Found %d files in the metadata\n', ntrajs);
+
+colors = jet(ntrajs); % get a color for each one
+% these next 2 lines make array in a type that can hold graphic objects
+% (not arrays of numbers like zeros or cell arrays)
+hbody = gobjects(ntrajs,1); 
 hwings = gobjects(ntrajs,1);
-for flyi = 1:ntrajs,
-  trk = data.exp.fly(flyi);
-  [hbody(flyi),hwings(flyi)] = DrawWingedFly(trk.x_px(fr),trk.y_px(fr),trk.theta_rad(fr),...
-    trk.a_px(fr),trk.b_px(fr),trk.xwingl_px(fr),trk.ywingl_px(fr),...
-    trk.xwingr_px(fr),trk.ywingr_px(fr),'style','ellipse',...
-    'Color',colors(flyi,:),'LineWidth',2);
+% loop over each track coresponding to a fly
+for flyi = 1:ntrajs
+    % get the track
+    trk = data.exp.fly(flyi);
+    % get locations based on the frame we want (fr)
+    x = trk.x_px(fr);
+    y = trk.y_px(fr);
+    fprintf('Drawing fly #%d at location x: %1.1f, y: %1.1f in pixels\n', flyi, x, y)
+    angle = trk.theta_rad(fr);
+    a =  trk.a_px(fr);
+    b = trk.b_px(fr);
+    x_wingl = trk.xwingl_px(fr);
+    y_wingl = trk.ywingl_px(fr);
+    xwingr = trk.xwingr_px(fr);
+    ywingr = trk.ywingr_px(fr);
+    color = colors(flyi,:);
+    % use all that data for the function DrawWingedFly that returns the
+    [hbody(flyi),hwings(flyi)] = DrawWingedFly( ...
+        x, y, angle, a, b, ...
+        x_wingl, y_wingl, xwingr, ywingr, ...
+        'style','ellipse', 'Color',color,'LineWidth',2);
 end
 axis image off;
-
+% worked until here, I have only one example movie so 
 
 %% plot trajectories
 
 data = LoadTracking(expdirs);
 nflies = size(data.summary.flies,1);
-T = max(data.summary.exps.nframes);
+T = max(data.summary.exps.nframes); % max number of frames in the movie
 nc = 5;
 nr = ceil(nflies/nc);
 mintimestamp = inf;
